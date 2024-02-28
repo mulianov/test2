fa90beaeb7e21da30ceb7f48cfef32275f0133f1
Mikhail Ulyanov <m.ulyanov@yadro.com>
2024-02-28 13:02:06 +0300

    update

--- test1.tcl
+++ test1.tcl
@@ -1,17 +1,17 @@
 # run_bft_kintex7_batch.tcl
-# bft sample design 
+# bft sample design
 # A Vivado script that demonstrates a very simple RTL-to-bitstream non-project batch flow
 #
-# NOTE:  typical usage would be "vivado -mode tcl -source run_bft_kintex7_batch.tcl" 
+# NOTE:  typical usage would be "vivado -mode tcl -source run_bft_kintex7_batch.tcl"
 #
 # STEP#0: define output directory area.
 #
-set outputDir ./_output             
+set outputDir ./_output
 file mkdir $outputDir
 #
 # STEP#1: setup design sources and constraints
 #
-# read_vhdl -library bftLib [ glob ./Sources/hdl/bftLib/*.vhdl ]         
+# read_vhdl -library bftLib [ glob ./Sources/hdl/bftLib/*.vhdl ]
 # read_vhdl ./Sources/hdl/bft.vhdl
 # read_verilog  [ glob ./Sources/hdl/*.v ]
 read_verilog -sv -verbose top.sv

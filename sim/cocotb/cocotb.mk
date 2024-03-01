872bda5bc887b002a085db4a6843f2d635d3038c
Mikhail Ulyanov <m.ulyanov@yadro.com>
2024-03-01 14:45:38 +0300

    update

--- sim/cocotb/cocotb.mk
+++ sim/cocotb/cocotb.mk
@@ -1,8 +1,12 @@
 COCOTB_BUILD_DIR = $(CUR_DIR)/build/cocotb
+COCOTB_SRC_DIR = $(CUR_DIR)/sim/cocotb
+
+.EXPORT_ALL_VARIABLES:
+PYTHONPYCACHEPREFIX=$(COCOTB_BUILD_DIR)
 
 cocotb.tb:
 	mkdir -p $(COCOTB_BUILD_DIR)
-	cd $(COCOTB_BUILD_DIR); pytest $(SIM_SRC_DIR)
+	cd $(COCOTB_BUILD_DIR); pytest $(COCOTB_SRC_DIR)
 
 cocotb.clean:
 	rm -rf $(COCOTB_BUILD_DIR)

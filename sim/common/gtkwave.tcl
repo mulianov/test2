84976becba7bb5624ec48aefb7a9bceeca8eb451
Mikhail Ulyanov <m.ulyanov@yadro.com>
2024-03-02 01:09:20 +0300

    update

--- sim/common/gtkwave.tcl
+++ sim/common/gtkwave.tcl
@@ -1,8 +1,13 @@
 set nfacs [ gtkwave::getNumFacs ]
 set all_facs [list]
+set index -1
+set filterKeyword "top_instance"
 for {set i 0} {$i < $nfacs } {incr i} {
     set facname [ gtkwave::getFacName $i ]
-    lappend all_facs "$facname"
+    set index [ string first $filterKeyword $facname  ]
+    if {$index != -1} {
+        lappend all_facs "$facname"
+    }
 }
 set num_added [ gtkwave::addSignalsFromList $all_facs ]
 puts "num signals added: $num_added"

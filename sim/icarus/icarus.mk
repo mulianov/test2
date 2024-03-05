2baa57acc9415505d2c1b18c015b863ad4e66d17
Mikhail Ulyanov <m.ulyanov@yadro.com>
2024-03-05 14:02:37 +0300

    update

--- sim/icarus/icarus.mk
+++ sim/icarus/icarus.mk
@@ -1,19 +1,21 @@
 ICARUS_BUILD_DIR = $(CUR_DIR)/build/icarus
 ICARUS_SRC_DIR = $(CUR_DIR)/sim/icarus
 
-icarus.tb:
+icarus.compile:
 	mkdir -p $(ICARUS_BUILD_DIR)
 	iverilog -o $(ICARUS_BUILD_DIR)/top.vvp -g2012 \
 		$(RTL_SRC_DIR)/top.sv $(SIM_SRC_DIR)/top_tb.sv
+
+icarus.run: icarus.compile
 	cd $(ICARUS_BUILD_DIR); vvp top.vvp -fst
 
-icarus.wave: icarus.tb
+icarus.wave: icarus.run
 	gtkwave -T $(SIM_COMMON_DIR)/gtkwave.tcl $(ICARUS_BUILD_DIR)/wave.fst
 
-icarus.vpi: icarus.tb
+icarus.vpi: icarus.compile
 	cd $(ICARUS_BUILD_DIR) ;\
 	iverilog-vpi $(ICARUS_SRC_DIR)/test_vpi.c ;\
-	vvp -M . -m test_vpi top.vvp
+	vvp -M. -mtest_vpi top.vvp
 
 icarus.clean:
 	rm -rf $(ICARUS_BUILD_DIR)
